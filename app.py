67fd68ccd0ba71e4f61f663cf8e04a80c2ed3224
Manikanta Loya <109620403+manikanta2704@users.noreply.github.com>
2024-04-21 06:33:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from nltk.corpus import stopwords
 import re
+import pyPDF2
 
 # Function to clean text
 def clean_text(text):
