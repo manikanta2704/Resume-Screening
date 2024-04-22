87015cd37cd6ee68d92a5bcd076e330bdebaa113
Manikanta Loya <109620403+manikanta2704@users.noreply.github.com>
2024-04-22 14:32:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import PyPDF2
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from nltk.corpus import stopwords
 import re
 import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from nltk.corpus import stopwords
 
 # Function to clean text
 @st.cache
@@ -15,13 +15,20 @@ def clean_text(text):
     :param text: The string to be cleaned
     :return: The cleaned string
     """
+    # Compile patterns for URLs and emails to speed up cleaning process
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
 
+    # Remove URLs
     cleaned_text = url_pattern.sub('', text)
+
+    # Remove emails
     cleaned_text = email_pattern.sub('', cleaned_text)
+
+    # Remove special characters (keeping only words and whitespace)
     cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
-    
+
+    # Remove stop words by filtering the split words of the text
     stop_words = set(stopwords.words('english'))
     cleaned_text = ' '.join(word for word in cleaned_text.split() if word.lower() not in stop_words)
 
@@ -32,51 +39,20 @@ def load_models():
     with open('tfidf.pkl', 'rb') as tfidf_file:
         tfidf_loaded = pickle.load(tfidf_file)
 
-    # Load the trained Logistic Regression classifier
+    # Load the trained logistic regression classifier
     with open('clf.pkl', 'rb') as clf_file:
         clf_loaded = pickle.load(clf_file)
 
     return tfidf_loaded, clf_loaded
 
-def predict_category(cleaned_resume_text, tfidf_vectorizer, classifier):
-    # Transform cleaned resume text using TF-IDF vectorizer
-    input_features = tfidf_vectorizer.transform([cleaned_resume_text])
-
-    # Make prediction using the loaded classifier
-    prediction_id = classifier.predict(input_features)[0]
-
-    # Map category ID to category name
-    category_mapping = {
-        15: "Java Developer", 23: "Testing", 8: "DevOps Engineer", 20: "Python Developer",
-        24: "Web Designing", 12: "HR", 13: "Hadoop", 3: "Blockchain", 10: "ETL Developer",
-        18: "Operations Manager", 6: "Data Science", 22: "Sales", 16: "Mechanical Engineer",
-        1: "Arts", 7: "Database", 11: "Electrical Engineering", 14: "Health and fitness",
-        19: "PMO", 4: "Business Analyst", 9: "DotNet Developer", 2: "Automation Testing",
-        17: "Network Security Engineer", 21: "SAP Developer", 5: "Civil Engineer", 0: "Advocate"
-    }
-
-    predicted_category = category_mapping.get(prediction_id, "Unknown")
-    return predicted_category
-
 def main():
+    
     st.title("Automated Resume Screening App")
 
-    # Load models (TF-IDF vectorizer and Logistic Regression classifier)
-    tfidf_loaded, clf_loaded = load_models()
+    # Use sidebar to navigate between pages (PDF and Text)
+    selected_page = st.sidebar.radio("Navigate", ["PDF", "Text"])
 
-    # Select the mode (Text or PDF)
-    selected_mode = st.sidebar.radio("Choose Input Mode", ["Text", "PDF"])
-
-    if selected_mode == "Text":
-        st.subheader("Enter Text Resume")
-        text_resume = st.text_area("Paste your Resume text here", height=300)
-
-        if st.button("Predict Category"):
-            cleaned_text = clean_text(text_resume)
-            predicted_category = predict_category(cleaned_text, tfidf_loaded, clf_loaded)
-            st.write(f"Predicted Category: {predicted_category}")
-
-    elif selected_mode == "PDF":
+    if selected_page == "PDF":
         st.subheader("Upload your Resume in .pdf format")
         pdf_file = st.file_uploader("Upload PDF", type=["pdf"])
 
@@ -87,10 +63,56 @@ def main():
                 page = pdf_reader.pages[page_num]
                 extracted_text += page.extract_text()
 
+            st.write("### Extracted Text from pdf:")
+            st.write(extracted_text)
+
             if extracted_text:
                 cleaned_text = clean_text(extracted_text)
-                predicted_category = predict_category(cleaned_text, tfidf_loaded, clf_loaded)
-                st.write(f"Predicted Category: {predicted_category}")
+                st.write("### Cleaned Text:")
+                st.write(cleaned_text)
+
+                if st.button("Predict job role from PDF"):
+                    tfidf_loaded, clf_loaded = load_models()
+                    input_features = tfidf_loaded.transform([cleaned_text])
+                    prediction_id = clf_loaded.predict(input_features)[0]
+
+                    category_mapping = {
+                        15: "Java Developer", 23: "Testing", 8: "DevOps Engineer", 20: "Python Developer",
+                        24: "Web Designing", 12: "HR", 13: "Hadoop", 3: "Blockchain", 10: "ETL Developer",
+                        18: "Operations Manager", 6: "Data Science", 22: "Sales", 16: "Mechanical Engineer",
+                        1: "Arts", 7: "Database", 11: "Electrical Engineering", 14: "Health and fitness",
+                        19: "PMO", 4: "Business Analyst", 9: "DotNet Developer", 2: "Automation Testing",
+                        17: "Network Security Engineer", 21: "SAP Developer", 5: "Civil Engineer", 0: "Advocate"
+                    }
+
+                    predicted_category = category_mapping.get(prediction_id, "Unknown")
+                    # Display the formatted message with predicted category in streamlit
+                    st.markdown(f"<p style='font-size:25px; font-weight:bold'>The Given Resume is best suited for the role of: <span style='color:orange'>{predicted_category}</span></p>", unsafe_allow_html=True)
+    elif selected_page == "Text":
+        st.subheader("Enter Text Resume")
+        text_resume = st.text_area("Paste your Resume text here", height=300)
+
+        # Automatically clean the text as the user types
+        cleaned_text = clean_text(text_resume)
+        st.write("### Cleaned Text:")
+        st.write(cleaned_text)
+
+        if st.button("Predict Category from Text"):
+            tfidf_loaded, clf_loaded = load_models()
+            input_features = tfidf_loaded.transform([cleaned_text])
+            prediction_id = clf_loaded.predict(input_features)[0]
+
+            category_mapping = {
+                15: "Java Developer", 23: "Testing", 8: "DevOps Engineer", 20: "Python Developer",
+                24: "Web Designing", 12: "HR", 13: "Hadoop", 3: "Blockchain", 10: "ETL Developer",
+                18: "Operations Manager", 6: "Data Science", 22: "Sales", 16: "Mechanical Engineer",
+                1: "Arts", 7: "Database", 11: "Electrical Engineering", 14: "Health and fitness",
+                19: "PMO", 4: "Business Analyst", 9: "DotNet Developer", 2: "Automation Testing",
+                17: "Network Security Engineer", 21: "SAP Developer", 5: "Civil Engineer", 0: "Advocate"
+            }
+
+            predicted_category = category_mapping.get(prediction_id, "Unknown")
+            st.markdown(f"<p style='font-size:25px; font-weight:bold'>The Given Resume is best suited for the role of: <span style='color:lightgreen'>{predicted_category}</span></p>", unsafe_allow_html=True)
 
 # Run the main function to start the app
 if __name__ == "__main__":
