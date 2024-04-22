15a84ba49dabb062894be05ec32352dac17ed09d
Manikanta Loya <109620403+manikanta2704@users.noreply.github.com>
2024-04-22 15:54:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,11 +133,12 @@ def main():
                     predicted_category = category_mapping.get(prediction_id, "Unknown")
                     st.markdown(f"<p style='font-size:25px; font-weight:bold'>Predicted Category: <span style='color:orange'>{predicted_category}</span></p>", unsafe_allow_html=True)
 
-                    recommendations = get_job_recommendations(predicted_category)
-                    if recommendations:
+                   recommendations = get_job_recommendations(predicted_category)
+                   if recommendations:
                         st.write("Job role recommendations:")
                         for job_role in recommendations:
-                            st.write(f"- {job_role}")
+                            st.write(f"- <span style='color:cyan'>{job_role}</span>", unsafe_allow_html=True)
+
 
     elif selected_page == "Text":
         st.subheader("Enter Text Resume")
@@ -165,11 +166,11 @@ def main():
             predicted_category = category_mapping.get(prediction_id, "Unknown")
             st.markdown(f"<p style='font-size:25px; font-weight:bold'>Predicted Category: <span style='color:lightgreen'>{predicted_category}</span></p>", unsafe_allow_html=True)
 
-            recommendations = get_job_recommendations(predicted_category)
-            if recommendations:
-                st.write("Job role recommendations:")
-                for job_role in recommendations:
-                    st.write(f"- {job_role}")
+             recommendations = get_job_recommendations(predicted_category)
+                   if recommendations:
+                        st.write("Job role recommendations:")
+                        for job_role in recommendations:
+                            st.write(f"- <span style='color:Magenta'>{job_role}</span>", unsafe_allow_html=True)
                     
     elif selected_page == "Resume Score":
         st.subheader("Calculate Resume Similarity Score")
